e9b02173973e599b245eacbcbf36c4f3821da3be
Eduardo Cintra <educin15@hotmail.com>
2025-01-13 12:54:20 -0300

    Creating the sample controllers, services and modules for entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { ClientsModule } from './clients/clients.module';
+import { OauthModule } from './oauth/oauth.module';
+import { AuditLogModule } from './audit-log/audit-log.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, RolesModule, ClientsModule, OauthModule, AuditLogModule],
   controllers: [],
   providers: [],
 })
